52f15d77bdfbfce4225e1643a268f743651c57fd
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-14 17:53:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4,
       rent_in_dollars: {
-        first:'',
+        one_piece_owned: 25,
         second: '',
         third: '',
         four: ''
